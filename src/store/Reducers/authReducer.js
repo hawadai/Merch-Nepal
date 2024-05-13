4bc615b5be7a4e8885f4924a2e3bb789a3534269
Aayush <hisenberg2248@gmail.com>
2024-05-13 12:44:07 +0545

    updated css bugs and errors

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -1,29 +1,25 @@
-import { createAsyncThunk,createSlice } from "@reduxjs/toolkit";
-
-export const admin_login =createAsyncThunk(
-    'auth/admin_login',
-    async (info) => {
-        try {
-           // const {data} = await api.post('/admin-login',info,{withCredentials:true});
-
-        } catch (error) {
-            
-        }
-    }
-) 
-export const authReducer =createSlice({
-    name: "auth",
-    initialState: {
-        successMessage: "",
-        errorMessage: "",
-        loader: false,
-        userInfo:""
-    },
-    reducers: {
-
-    },
-    extraReducers:() => {
-
-    }
-})
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
+export const admin_login = createAsyncThunk(
+  "auth/admin_login",
+  async (info) => {
+    try {
+      const { data } = await api.post("/admin-login", info, {
+        withCedentials: true,
+      });
+      console.log(data);
+    } catch (error) {}
+  }
+);
+export const authReducer = createSlice({
+  name: "auth",
+  initialState: {
+    successMessage: "",
+    errorMessage: "",
+    loader: false,
+    userInfo: "",
+  },
+  reducers: {},
+  extraReducers: () => {},
+});
 export default authReducer.reducer;
--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -22,55 +22,32 @@ const AdminLogin = () => {
   };
 
   return (
-    <div className="min-w-screen min-h-screen bg-[#000000] flex justify-center items-center">
-      <div className="w-[350px] text-[#befe2c] p-2">
-        <div className="bg-[#6f68d1]p-4 rounded-md">
-          <div className="h-[70px]flex justify-center items-center">
-            <img
-              className="w-full h-full"
-              src="http://localhost:3000/images/logo2.png"
-              alt="image"
-            />
-            <div className="w-[100%] bg-[#66ff00] h-[2px] "></div>
-            <br />
-            <h2 className='text-xl mb-3 font-bold'>Admin Login</h2>
+    <div className='min-w-screen min-h-screen bg-[#000000] flex justify-center items-center' >
+    <div className='w-[350px] text-[#befe2c] p-2'>
+    <img className ='w-full h-full' src= "http://localhost:3000/images/logo2.png" alt="image" />
+    <div className='w-[100%] bg-[#befe2c] h-[2px] '></div>
+    <br />
+    <h2 className='text-xl mb-3 font-bold'>Admin Login</h2>
 
-          </div>
-          <form onSubmit={submit}>
-            <div className="flex flex-col w-full gap-1 mb-3">
-              <label htmlFor="name">Email</label>
-              <input
-                onChange={inputHandle}
-                value={state.email}
-                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
-                type="email"
-                name="email"
-                placeholder="Email"
-                id="email"
-                required
-              />
-            </div>
-            <div className="flex flex-col w-full gap-1 mb-3">
-              <label htmlFor="password">Password</label>
-              <input
-                onChange={inputHandle}
-                value={state.password}
-                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
-                type="password"
-                name="Password"
-                placeholder="Password"
-                id="password"
-                required
-              />
-            </div>
+<form onSubmit={submit}>
+   
+  <div className='flex flex-col w-full gap-1 mb-3 font-semibold'>
+      <label htmlFor="email">Email</label>
+      <input onChange={inputHandle} value={state.email}  className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="email" name='email' placeholder='Email' id='email' required />
 
-            <button className="bg-slate-800 w-full hover:shadow-green-300/hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
-              Log In
-            </button>
+  </div>
+
+  <div className='flex flex-col w-full gap-1 mb-3 font-semibold'>
+      <label htmlFor="password">Password</label>
+      <input onChange={inputHandle} value={state.password}  className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="password" name='password' placeholder='Password' id='password' required />
+  </div>
+
+
+  <button className='bg-[#2b5f53] w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Sign In</button>
           </form>
-        </div>
-      </div>
-    </div>
+          </div>
+          </div>
+
   );
 };
 
