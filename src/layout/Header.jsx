28ad091b2ba3829187b46e2f7b1479063212d44a
Aayush <hisenberg2248@gmail.com>
2024-05-18 15:03:35 +0545

    made the webpages responsive

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({showSidebar, setShowSidebar}) => {
         <div className='fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40'>
           <div className='ml-0 lg:ml-[260px] rounded-lg h-[65px] flex justify-between items-center bg-[#135a76] px-5 transition-all'>
 
-        <div onClick={() => setShowSidebar(!showSidebar)} className='w-[35px] flex lg:hidden h-[35px] rounded-sm bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer ' >
+        <div onClick={() => setShowSidebar(!showSidebar)} className='w-[35px] flex lg:hidden h-[35px] rounded-sm bg-[#135a76] shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer ' >
           <span><FaList/></span>
         </div>
 
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import { getNav } from "../navigation/index";
 import { IoLogOut } from "react-icons/io5";
 
-const Sidebar = () => {
+const Sidebar = ({showSidebar, setShowSidebar}) => {
   const { pathname } = useLocation();
   const [allNav, setAllNav] = useState([]);
   useEffect(() => {
@@ -14,10 +14,11 @@ const Sidebar = () => {
 
   return (
     <div>
-      <div></div>
+      <div onClick={()=> setShowSidebar(false)} className={`fixed duration-200 ${!showSidebar ? 'invisible': 'visible'} w-screen h-screen bg-[#aed6f6] top-0 left-0 z-10`}>
+      </div>
 
       <div
-        className={`w-[260px] fixed bg-[#000000] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all `}
+        className={`w-[260px] fixed bg-[#000000] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${showSidebar ? 'left-0': '-left-[260px] lg:left-0'}`}
       >
         <div className="h-[80px] flex justify-center items-center">
           <Link to="/" className="w-[200px] h-[80px]">
