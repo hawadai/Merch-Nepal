caf9b0c65615755864e19f72f00af9c38f7e85d4
Aayush <hisenberg2248@gmail.com>
2024-05-18 16:38:02 +0545

    worked on seller activate and deactivate page fontend

--- src/views/admin/DeactiveSellers.jsx
+++ src/views/admin/DeactiveSellers.jsx
@@ -3,25 +3,26 @@ import { Link } from 'react-router-dom';
 import Pagination from '../Pagination';
 import { FaEye } from "react-icons/fa";
 
-const Sellers = () => {
+const DeactiveSellers = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [searchValue, setSearchValue] = useState('')
     const [parPage, setParPage] = useState(5)
     const [show, setShow] =  useState(false)
 
     return (
-        
         <div className='px-2 lg:px-7 pt-5'>
-                         <h1 className='text-[20px] font-bold mb-3'>Sellers</h1>
-             <div className='w-full p-4 bg-[#1d1a41] rounded-lg'>
+            
+      <h1 className='text-[20px] font-bold mb-3'>Deactive Sellers</h1>
+             
+             <div className='w-full p-4 bg-[#1d1a41] rounded-md'>
             
              <div className='flex justify-between items-center'>
-                    <select onChange={(e) => setParPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#c8ff2d] border border-slate-700 rounded-md text-[#000000]'>
+                    <select onChange={(e) => setParPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#c8ff2d] border border-slate-700 rounded-md text-black'>
                         <option value="5">5</option>
                         <option value="10">10</option>
                         <option value="20">20</option> 
                     </select>
-                    <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#c8ff2d] border border-slate-700 rounded-md text-[#010101]' type="text" placeholder='search' /> 
+                    <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#c8ff2d] border border-slate-700 rounded-md text-black' type="text" placeholder='search' /> 
                 </div>
 
                 <div className='relative overflow-x-auto'>
@@ -31,11 +32,9 @@ const Sellers = () => {
             <th scope='col' className='py-3 px-4'>Num</th>
             <th scope='col' className='py-3 px-4'>Image</th>
             <th scope='col' className='py-3 px-4'>Name</th>
-            <th scope='col' className='py-3 px-4'>Shop Name</th> 
-            <th scope='col' className='py-3 px-4'>Payment Status</th> 
             <th scope='col' className='py-3 px-4'>Email</th> 
-            <th scope='col' className='py-3 px-4'>State</th> 
-            <th scope='col' className='py-3 px-4'>District</th> 
+            <th scope='col' className='py-3 px-4'>Payment Status</th> 
+            <th scope='col' className='py-3 px-4'>Seller Status</th>  
             <th scope='col' className='py-3 px-4'>Action</th> 
         </tr>
         </thead>
@@ -45,21 +44,20 @@ const Sellers = () => {
                 [1,2,3,4,5].map((d, i) => <tr key={i}>
                 <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d}</td>
                 <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                    <img className='w-[45px] h-[45px]' src={`http://localhost:3000/images/categories/${d}.jpg`} alt="" />
+                    <img className='w-[45px] h-[45px]' src={`http://localhost:3000/images/category/${d}.jpg`} alt="" />
                 </td>
                 <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Aayush </td>
-                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Merch Nepal</td>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>aayush@gmail.com</td>
                 <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
                     <span>Pending</span> </td>
-                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>aayush@gmail.com </td>
 
-                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d} </td>
-
-                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'> empty </td>
+               <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
+                    <span>Deactive</span> </td>
+                 
                  
                 <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
                     <div className='flex justify-start items-center gap-4'>
-                    <Link className='p-[6px] bg-[#33a01b] rounded hover:shadow-lg hover:shadow-green-500/50'> <FaEye /> </Link> 
+                    <Link className='p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50'> <FaEye /> </Link> 
                     
                     </div>
                     
@@ -93,4 +91,4 @@ const Sellers = () => {
     );
 };
 
-export default Sellers;
+export default DeactiveSellers;
