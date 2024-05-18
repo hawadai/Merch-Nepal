1e343fa205138be22d2e734d81bb5cdc2a5b7f84
Aayush <hisenberg2248@gmail.com>
2024-05-18 15:57:58 +0545

    made the interface for sellers page

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -2,6 +2,7 @@ import { lazy } from "react";
 const AdminDashboard = lazy(() => import("../../views/admin/AdminDashboard"));
 const Orders = lazy(() => import("../../views/admin/Orders"));
 const Category = lazy(() => import("../../views/admin/Category"));
+const Sellers = lazy(() => import("../../views/admin/Sellers"));
 
 
 export const adminRoutes = [
@@ -20,4 +21,10 @@ export const adminRoutes = [
     element: <Category/>,
     role: "admin",
   },
+  {
+    path: "admin/dashboard/sellers",
+    element: <Sellers/>,
+    role: "admin",
+  },
+
 ];
