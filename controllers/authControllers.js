fafbbfdcee9a5648fb4a4c64e5cf521d3c4418e5
Aayush <hisenberg2248@gmail.com>
2024-11-25 10:23:28 +0545

    worked on the seller register backend

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,61 +1,80 @@
-const adminModel = require('../models/adminModel')
-const { responseReturn } = require('../utiles/response')
-const bcrpty = require('bcrypt')
-const { createToken } = require('../utiles/tokenCreate')
+const adminModel = require("../models/adminModel");
+const { responseReturn } = require("../utiles/response");
+const bcrpty = require("bcrypt");
+const { createToken } = require("../utiles/tokenCreate");
+const sellerModel = require("../models/sellerModel");
+const sellerCustomerModel = require("../models/chat/sellerCustomerModel");
 
-class authControllers{
-    admin_login = async(req,res) => {
-        const {email,password} = req.body
-        try {
-            const admin = await adminModel.findOne({email}).select('+password')
-            // console.log(admin)
-            if (admin) {
-                const match = await bcrpty.compare(password, admin.password)
-                // console.log(match)
-                if (match) {
-                    const token = await createToken({
-                        id : admin.id,
-                        role : admin.role
-                    })
-                    res.cookie('accessToken',token,{
-                        expires : new Date(Date.now() + 7*24*60*60*1000 )
-                    }) 
-                    responseReturn(res,200,{token,message: "Login Success"})
-                } else {
-                    responseReturn(res,404,{error: "Password Wrong"})
-                }
-               
-            } else {
-                responseReturn(res,404,{error: "Email not Found"})
-            }
-            
-        } catch (error) {
-            responseReturn(res,500,{error: error.message})
+class authControllers {
+  admin_login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const admin = await adminModel.findOne({ email }).select("+password");
+      // console.log(admin)
+      if (admin) {
+        const match = await bcrpty.compare(password, admin.password);
+        // console.log(match)
+        if (match) {
+          const token = await createToken({
+            id: admin.id,
+            role: admin.role,
+          });
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "Login Success" });
+        } else {
+          responseReturn(res, 404, { error: "Password Wrong" });
         }
- 
+      } else {
+        responseReturn(res, 404, { error: "Email not Found" });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
     }
-    seller_register = async(req, res) => {
-         const {email,name,password} = req.body
+  };
+  seller_register = async (req, res) => {
+    const { email, name, password } = req.body;
+    try {
+      const getUser = await sellerModel.findOne({ email });
+      if (getUser) {
+        responseReturn(res, 404, { error: "Email Already Exit" });
+      } else {
+        const seller = await sellerModel.create({
+          name,
+          email,
+          password: await bcrpty.hash(password, 10),
+          method: "manually",
+          shopInfo: {},
+        });
+        await sellerCustomerModel.create({
+          myId: seller.id
+    })
+    const token = await createToken({ id : seller.id, role: seller.role })
+    res.cookie('accessToken',token, {
+     expires : new Date(Date.now() + 7*24*60*60*1000 )
+    })
+    responseReturn(res,201,{token,message: 'Register Success'})
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error:'Server Error, Please try again later' });
     }
+  };
 
-    getUser = async (req, res) => {
-        const {id, role} = req;
+  getUser = async (req, res) => {
+    const { id, role } = req;
 
-        try {
-            if (role === 'admin') {
-                const user = await adminModel.findById(id)
-                responseReturn(res, 200, {userInfo : user})
-            }else {
-                console.log('Seller Info')
-            }
-            
-        } catch (error) {
-            console.log(error.message)
-        }
-
-
-    } 
-
+    try {
+      if (role === "admin") {
+        const user = await adminModel.findById(id);
+        responseReturn(res, 200, { userInfo: user });
+      } else {
+        console.log("Seller Info");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 }
 
-module.exports = new authControllers()
+module.exports = new authControllers();
