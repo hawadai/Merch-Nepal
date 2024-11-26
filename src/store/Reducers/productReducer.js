d6c525bf6782935a024ea7681c7d6738a985ccad
Aayush <hisenberg2248@gmail.com>
2024-11-26 18:16:57 +0545

    image edit fixed

--- src/store/Reducers/productReducer.js
+++ src/store/Reducers/productReducer.js
@@ -60,7 +60,30 @@ export const update_product = createAsyncThunk(
   "product/update_product",
   async (product, { rejectWithValue, fulfillWithValue }) => {
     try {
-      const { data } = await api.post('/product-update', product, {
+      const { data } = await api.post("/product-update", product, {
+        withCredentials: true,
+      });
+      console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data)
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const product_image_update = createAsyncThunk(
+  "product/product_image_update",
+  async (
+    { oldImage, newImage, productId },
+    { rejectWithValue, fulfillWithValue }
+  ) => {
+    try {
+      const formData = new FormData();
+      formData.append("oldImage", oldImage);
+      formData.append("newImage", newImage);
+      formData.append("productId", productId);
+      const { data } = await api.post("/product-image-update", formData, {
         withCredentials: true,
       });
       console.log(data);
@@ -110,17 +133,20 @@ export const productReducer = createSlice({
       })
       .addCase(update_product.pending, (state, { payload }) => {
         state.loader = true;
-    })
-    .addCase(update_product.rejected, (state, { payload }) => {
+      })
+      .addCase(update_product.rejected, (state, { payload }) => {
         state.loader = false;
-        state.errorMessage = payload.error
-    }) 
-    .addCase(update_product.fulfilled, (state, { payload }) => {
+        state.errorMessage = payload.error;
+      })
+      .addCase(update_product.fulfilled, (state, { payload }) => {
         state.loader = false;
-        state.product = payload.product 
-        state.successMessage = payload.message 
-         
-    });
+        state.product = payload.product;
+        state.successMessage = payload.message;
+      })
+      .addCase(product_image_update.fulfilled, (state, { payload }) => {
+        state.product = payload.product;
+        state.successMessage = payload.message;
+      });
   },
 });
 export const { messageClear } = productReducer.actions;
